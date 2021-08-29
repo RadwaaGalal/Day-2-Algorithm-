cd8b3a323bc4ec91141052d613a5fd78e35bf9c0
RadwaaGalal <radwagalal318@gmail.com>
2021-08-29 22:27:17 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#include<time.h>
+int size = 100000;
+int array[100000];
+//make array random of 1000000 elements
+void Random_number(int array[])
+{
+  int i;
+  srand(time(NULL));
+  for (i=0 ; i<size ; i++)
+  {
+       array[i] = rand() % 100000 + 1;
+
+  }
+}
+//Function to sort an array using insertion sort
+void insertion_sort(int arr[] , int size)
+{
+    int i, Comp, j;
+    for (i = 1; i < size; i++) {
+        Comp = arr[i];
+        j = i - 1;
+    while (j >= 0 && arr[j] > Comp) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+        }
+        arr[j + 1] = Comp;
+    }
+    printf("\nThe array is sorted using insertion sort in %u seconds\n", clock());
+}
+//function to print an array of size (size)
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+//function to swap elements in in quick sort
+void swap(int *i, int *j) {
+  int temp = *i;
+  *i = *j;
+  *j = temp;
+}
+
+int partition(int array[], int low, int high) {
+
+  int pivot = array[high];
+  int i = (low - 1);
+  int j;
+  for (j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    int pi = partition(array, low, high);
+
+    quickSort(array, low, pi - 1);
+    quickSort(array, pi + 1, high);
+  }
+}
+
+int main()
+{
+  	int i;
+	srand(time(NULL));
+	for (i = 0; i < size; i++) {
+		array[i] = rand() % 100000 + 1;
+	}
+	Random_number(array);
+
+	printf("Array of 100000 random numbers is initialized\n");
+
+
+	quickSort(array ,0, size - 1);
+	printf("The array is sorted using quick sort in %u seconds", clock());
+
+    insertion_sort(array, size);
+    return 0;
+}
